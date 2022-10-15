ebe0b43df642392bb8b3b2e35f7b5cf0049fb773
Tobias Sörensson <tobbesoren@gmail.com>
2022-10-15 15:09:58 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, Integer> myMap = new HashMap<>();
+
+        myMap.e
+    }
+}
